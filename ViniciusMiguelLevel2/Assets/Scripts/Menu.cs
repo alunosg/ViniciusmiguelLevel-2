8a69229cfec582e23d949d6da3f5216fb07ed2c4
JOPE3778 <jp7maciel@gmail.com>
2026-04-18 10:32:24 -0300

    Start,Credits,Quit

---
+++ ViniciusMiguelLevel2/Assets/Scripts/Menu.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+  [SerializeField] private GameObject creditsPanel;
+
+  public void strartGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+  public void QuitGame()
+    {
+        Application.Quit();
+    } 
+
+  public void OpenCredits()
+    {
+        creditsPanel.SetActive(true);
+    }
+  public void CloseCredits()
+    {
+        creditsPanel.SetActive(false);
+    }
+}
