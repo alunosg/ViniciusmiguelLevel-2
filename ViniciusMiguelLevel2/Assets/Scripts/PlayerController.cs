e60a3f7b9838ec5ea4f66539e65f2c7bf33fb458
JOPE3778 <jp7maciel@gmail.com>
2026-05-09 10:38:42 -0300

    animação de morte

--- ViniciusMiguelLevel2/Assets/Scripts/PlayerController.cs
+++ ViniciusMiguelLevel2/Assets/Scripts/PlayerController.cs
@@ -1,5 +1,8 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
 
 public class PlayerController : MonoBehaviour
 {
@@ -11,6 +14,8 @@ public class PlayerController : MonoBehaviour
     public Animator anim;
     public float attackDuration = 1.0f;
     public float airAttackDuration = 1.0f;
+    public float hitDuration = 0.25f;
+    public float deathDuration = 2.0f;
 
     public GameObject attackFX, attackHitFX,  hitFX, deathFX;
     public Transform attackPoint;
@@ -18,6 +23,39 @@ public class PlayerController : MonoBehaviour
     private Vector2 moveInput;
     private bool grounded = false;
     private bool locked = false;
+
+    private float maxHp;
+    public float hp = 3;
+    public Image hpBar;
+
+    void Start()
+    {
+         maxHp = hp;
+    }
+
+    public void GetHit(float damage, float push, Vector3 pos)
+    {
+        hp -= damage;
+        hpBar.fillAmount = Mathf.Max(0, hp / maxHp);
+        locked = true;
+        CancelInvoke("Unlock");
+        if (hitFX) Instantiate(hitFX, transform.position, transform.rotation);
+
+        if (hp > 0)
+        {
+            anim.SetTrigger("Hit");
+            Invoke(nameof(Unlock), hitDuration);
+            rig.linearVelocity = (transform.position - pos).normalized * push; 
+        }
+        else
+        {
+            anim.SetTrigger("Death");
+            Invoke(nameof(Reload), deathDuration);
+            rig.linearVelocity = Vector2.zero; 
+        }
+    }
+
+    private void Reload() => SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
 
     public void MoveInput(InputAction.CallbackContext contexte)
     {
@@ -44,6 +82,7 @@ public class PlayerController : MonoBehaviour
     {
          if (!locked && contexte.started)
         {
+            if(grounded) rig.linearVelocity = Vector2.zero;
             anim.SetTrigger("Attack");
             locked = true;
             if(attackFX) Instantiate(attackFX, attackPoint.position, attackPoint.rotation);
@@ -74,12 +113,15 @@ public class PlayerController : MonoBehaviour
     private void FixedUpdate()
     {
         DetectGround();
-
-        rig.linearVelocity = new Vector2
+        if(!locked)
+        {
+            rig.linearVelocity = new Vector2
 
             (
-                locked && grounded ? 0 : speed * moveInput.x,
+                speed * moveInput.x,
                 rig.linearVelocity.y
             );
+        }
+
     }
 }
